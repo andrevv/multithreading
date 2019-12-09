eebcc15c954a297fedca7406a8722883f367d68b
Andrey Vladykov <avladykov@outlook.com>
2019-12-09 12:58:08 +0300

    Add Monitor.TryEnter() to prevent deadlocks.

--- Multithreading.Synchronization.Deadlock/Program.cs
+++ Multithreading.Synchronization.Deadlock/Program.cs
@@ -7,6 +7,12 @@ namespace Multithreading.Synchronization.Deadlock
     {
         private static void Main()
         {
+//            Deadlock();
+            DeadlockPrevention();
+        }
+
+        private static void Deadlock()
+        {
             var a = new object();
             var b = new object();
 
@@ -15,7 +21,7 @@ namespace Multithreading.Synchronization.Deadlock
                 lock (a)
                 {
                     Console.WriteLine("Thread A acquired the 'a' lock.");
-                    
+
                     Thread.Sleep(1000);
                     lock (b)
                     {
@@ -29,13 +35,104 @@ namespace Multithreading.Synchronization.Deadlock
                 lock (b)
                 {
                     Console.WriteLine("Thread B acquired the 'b' lock.");
-                    
+
                     Thread.Sleep(1000);
                     lock (a)
                     {
                         Console.WriteLine("Thread B acquired the 'a' lock.");
                     }
                 }
+            });
+
+            threadA.Start();
+            threadB.Start();
+            threadA.Join();
+            threadB.Join();
+
+            Console.WriteLine("Done");
+        }
+
+        private static void DeadlockPrevention()
+        {
+            var a = new object();
+            var b = new object();
+
+            var threadA = new Thread(() =>
+            {
+                lock (a)
+                {
+                    Console.WriteLine("Thread A acquired the 'a' lock.");
+
+                    lock (b)
+                    {
+                        Console.WriteLine("Thread A acquired the 'b' lock.");
+                        
+                        //
+                        // Do work
+                        //
+                        
+                        Thread.Sleep(5000);
+                    }
+                }
+            });
+
+            var threadB = new Thread(() =>
+            {
+                var acquiredA = false;
+                var acquiredB = false;
+                var timeout = TimeSpan.FromSeconds(1);
+                const int maxRetries = 10;
+
+                for (var retry = 0; retry < maxRetries; retry++)
+                {
+                    try
+                    {
+                        acquiredB = Monitor.TryEnter(b, timeout);
+                        if (acquiredB)
+                        {
+                            Console.WriteLine("Thread B acquired the 'b' lock.");
+
+                            try
+                            {
+                                acquiredA = Monitor.TryEnter(a, timeout);
+                                if (acquiredA)
+                                {
+                                    Console.WriteLine("Thread B acquired the 'a' lock.");
+
+                                    //
+                                    // Do work
+                                    //
+                                    
+                                    return;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Thread B did not acquire the 'a' lock.");
+                                }
+                            }
+                            finally
+                            {
+                                if (acquiredA)
+                                {
+                                    Monitor.Exit(a);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Thread B did not acquire the 'b' lock.");
+                        }
+                    }
+                    finally
+                    {
+                        if (acquiredB)
+                        {
+                            Monitor.Exit(b);
+                        }
+                    }
+                }
+
+                Console.WriteLine("Thread B could not acquire locks.");
             });
 
             threadA.Start();
